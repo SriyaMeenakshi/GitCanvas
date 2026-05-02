756ee10b6011363957f4536ebde5c7743e2afe84
Aditya8369 <mahajan2005aditya@gmail.com>
2026-05-02 18:15:28 +0530

    refactor: Duplicate Language Color Mapping

--- generators/repo_card.py
+++ generators/repo_card.py
@@ -1,6 +1,7 @@
 import svgwrite
 from themes.styles import THEMES
 from .svg_base import create_svg_base
+from utils.constants import LANGUAGE_COLORS
 
 def draw_repo_card(data, theme_name="Default", custom_colors=None, sort_by="stars", limit=5):
     """
@@ -82,40 +83,7 @@ def draw_repo_card(data, theme_name="Default", custom_colors=None, sort_by="star
         # Language badge (if available)
         language = repo.get("language", "Unknown") or "Unknown"
         if language != "Unknown":
-            # Language color dot
-            lang_colors = {
-                "Python": "#3776ab",
-                "JavaScript": "#f7df1e",
-                "TypeScript": "#3178c6",
-                "Java": "#b07219",
-                "C++": "#f34b7d",
-                "C": "#555555",
-                "Go": "#00add8",
-                "Rust": "#dea584",
-                "Ruby": "#701516",
-                "PHP": "#4F5D95",
-                "Swift": "#ffac45",
-                "Kotlin": "#A97BFF",
-                "HTML": "#e34c26",
-                "CSS": "#563d7c",
-                "Shell": "#89e051",
-                "R": "#198CE7",
-                "Scala": "#c22d40",
-                "Dart": "#00B4AB",
-                "Elixir": "#6e4a7e",
-                "Clojure": "#db5855",
-                "Haskell": "#5e5086",
-                "Lua": "#000080",
-                "Perl": "#0298c3",
-                "Julia": "#a270ba",
-                "Erlang": "#b83998",
-                "Objective-C": "#438eff",
-                "CoffeeScript": "#244776",
-                "Vim script": "#199f4b",
-                "TeX": "#3D6117",
-                "Jupyter Notebook": "#DA5B0B"
-            }
-            lang_color = lang_colors.get(language, icon_color)
+            lang_color = LANGUAGE_COLORS.get(language, icon_color)
             
             dwg.add(dwg.circle(
                 center=(30, y + 38),
---
+++ utils/constants.py
@@ -0,0 +1,38 @@
+"""
+Centralized constants for the project.
+
+Put shared mappings here to avoid duplication across generators.
+"""
+
+LANGUAGE_COLORS = {
+    "Python": "#3776ab",
+    "JavaScript": "#f7df1e",
+    "TypeScript": "#3178c6",
+    "Java": "#b07219",
+    "C++": "#f34b7d",
+    "C": "#555555",
+    "Go": "#00add8",
+    "Rust": "#dea584",
+    "Ruby": "#701516",
+    "PHP": "#4F5D95",
+    "Swift": "#ffac45",
+    "Kotlin": "#A97BFF",
+    "HTML": "#e34c26",
+    "CSS": "#563d7c",
+    "Shell": "#89e051",
+    "R": "#198CE7",
+    "Scala": "#c22d40",
+    "Dart": "#00B4AB",
+    "Elixir": "#6e4a7e",
+    "Clojure": "#db5855",
+    "Haskell": "#5e5086",
+    "Lua": "#000080",
+    "Perl": "#0298c3",
+    "Julia": "#a270ba",
+    "Erlang": "#b83998",
+    "Objective-C": "#438eff",
+    "CoffeeScript": "#244776",
+    "Vim script": "#199f4b",
+    "TeX": "#3D6117",
+    "Jupyter Notebook": "#DA5B0B",
+}
